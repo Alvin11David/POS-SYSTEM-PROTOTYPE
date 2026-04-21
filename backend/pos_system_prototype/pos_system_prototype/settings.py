5edb455ff6affa9b19d5f70ea8ffd2fe39ca38d8
atreavez <alvin69david@gmail.com>
2026-04-21 12:28:23 +0300

    Created the dist folder

--- backend/pos_system_prototype/pos_system_prototype/settings.py
+++ backend/pos_system_prototype/pos_system_prototype/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1", "testserver"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,7 +119,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+# CORS settings for Netlify frontend (allow all for now, restrict in production)
+CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
